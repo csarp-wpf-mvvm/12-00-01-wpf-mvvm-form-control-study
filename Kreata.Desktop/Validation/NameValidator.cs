7bec4654ea6d3f6ee954fbb1992b572dbf68a8af
Gyuris Csaba <gycsaba72@gmail.com>
2024-03-18 15:22:39 +0100

    Initial commit

---
+++ Kreata.Desktop/Validation/NameValidator.cs
@@ -0,0 +1,35 @@
+﻿using Kreta.Desktop.Validation.ValidationRules;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace Kreta.Desktop.Validation
+{
+    public class NameValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string)
+            {
+                string nameToValidate = (string)value;
+                NameValidationRules nvr = new NameValidationRules(nameToValidate);
+                if (nvr.IsNameShort)
+                {
+                    return new ValidationResult(false, "A név túl rövid!");
+                }
+                if (!nvr.IsFirstLetterUppercase)
+                {
+                    return new ValidationResult(false, "A név első betűje nagybetű kell legyen!");
+                }
+                if (!nvr.IsOtherLetterLowercase)
+                {
+                    return new ValidationResult(false, "Csak a név első betűje lehet nagybetű!");
+                }
+                if (!nvr.IsOnlyLetters)
+                {
+                    return new ValidationResult(false, "A név csak betűből állhat!");
+                }
+            }
+            return new ValidationResult(true, "");
+        }
+    }
+}
